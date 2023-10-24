3b93d2aa4e6e42595098651b7a2779c693a76f48
Tagada216 <nicolasdelcourt10@gmail.com>
2023-10-24 15:56:20 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import CardStoreComponent from "./components/CardStoreComponent.vue";
+import { ref, onBeforeMount } from "vue";
+import { storesData } from "./store";
+
+let displayInformations = ref(false);
+
+const stores = storesData.features;
+let selectedStore = ref({});
+
+function loadStore() {
+  const index = Math.floor(Math.random() * stores.length);
+  selectedStore.value = stores[index].properties;
+}
+
+onBeforeMount(() => {
+  loadStore();
+});
+</script>
+
+<template>
+  <div class="container">
+    <h1 id="magasinNom">{{ selectedStore.nom }}</h1>
+    <CardStoreComponent
+      :visible="displayInformations"
+      :storeData="selectedStore"
+      @close="displayInformations = false"
+    />
+  </div>
+  <button
+    v-if="!displayInformations"
+    class="more_button"
+    @click="displayInformations = !displayInformations"
+  >
+    Voir plus
+  </button>
+  <button class="more_button" @click="loadStore">Nouveau nom</button>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+}
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+#magasinNom {
+  font-size: 3.2em;
+  color: #dbdbe3;
+  text-decoration: inherit;
+  font-family: "Josefin Sans", sans-serif;
+  animation: flottaison 3s infinite;
+}
+@keyframes flottaison {
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-20px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+.more_button {
+  font-size: 1.5rem;
+  font-family: "Pacifico", cursive;
+}
+</style>
---
+++ src/components/CardStoreComponent.vue
@@ -0,0 +1,66 @@
+<template>
+  <div v-if="visible" class="card">
+    <div class="data">
+      <p>Nom : {{ storeData.nom }}</p>
+    </div>
+    <div class="data">
+      <p>Adresse : {{ storeData.addresse }}</p>
+    </div>
+    <div>
+      <button @click="emit('close')">
+        <svg
+          width="30px"
+          height="30px"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="12" cy="12" r="10" stroke="#1C274C" stroke-width="1.5" />
+          <path
+            d="M14.5 9.50002L9.5 14.5M9.49998 9.5L14.5 14.5"
+            stroke="#1C274C"
+            stroke-width="1.5"
+            stroke-linecap="round"
+          />
+        </svg>
+      </button>
+    </div>
+    <a
+      href="https://www.data.gouv.fr/fr/datasets/coordonnees-des-enseignes-de-coiffure-comportant-des-jeux-de-mots-cocasses-dans-leur-appellation/"
+      target="_blank"
+      >Dataset
+    </a>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from "vue";
+
+defineProps({
+  visible: {
+    type: Boolean,
+    required: false,
+  },
+  storeData: {
+    type: Object,
+  },
+});
+
+const emit = defineEmits(["close"]);
+</script>
+
+<style scoped>
+.card {
+  --size: clamp(300px, 600px);
+  width: var(--size);
+  background-color: beige;
+  border-radius: 15px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
+.data {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  font-family: "Josefin Sans", sans-serif;
+}
+</style>
